b0a9828335a3cacb17e895d3c3d4f965313bd524
TJ Bourke <tjbourke1@gmail.com>
2019-09-06 00:20:43 +0000

    Updated .env.example and my user seeder.

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -7,16 +7,7 @@ use Illuminate\Support\Facades\DB;
 class UserSeeder extends Seeder
 {
 
-    protected $admins = [
-        [
-            'name' => 'TJ Bourke',
-            'email' => 'tbourke@flexngate-mi.com' // i dont know your personal email
-        ],
-        [
-            'name' => 'Kyle Burleigh',
-            'email' => 'kyle.d.burleigh@gmail.com',
-        ],
-    ];
+    protected $admins;
 
     /**
      * Run the database seeds.
@@ -25,6 +16,18 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        $this->admins = [
+            [
+                'name' => 'TJ Bourke',
+                'email' => 'tjbourke1@gmail.com',
+                'password' => bcrypt('password'),
+            ],
+            [
+                'name' => 'Kyle Burleigh',
+                'email' => 'kyle.d.burleigh@gmail.com',
+            ],
+        ];
+
         foreach ($this->admins as $admin) {
             if (!DB::table('users')->where('email', $admin['email'])->exists()) {
                 factory(User::class)->create($admin);
